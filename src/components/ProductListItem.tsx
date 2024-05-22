ef3a7f68f4912f8f8de3e6a0ac6c1be0c23aafc1
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-05-23 01:47:02 +0200

    init expo project

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,11 @@
+
+
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+export const defaultPizzaImage =
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+
+export default function ProductListItem({  }){
+  return ( 
+        null
+  );}
