91329e6d9fafb338092a9b690d0a4da377f8f3b8
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-05-23 16:04:23 +0200

    Added state to select Piza size , styled, Product details screen, Wip: adding a buttton custom  comp

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,68 @@
-import { View,Text } from "@/components/Themed";
+import { View, Text } from "@/components/Themed";
 import { Stack, useLocalSearchParams } from "expo-router";
-
+import products from '../../../../assets/data/products';
+import { Image, Pressable, StyleSheet } from "react-native";
+import { useState } from "react";
+import Button from "@/components/Button";
 export default function TabOneScreen() {
-    const {id} = useLocalSearchParams();
-    
-    return(
-        <View>
-            <Stack.Screen options={{title: "Details"}} />
-            <Text>Product Details : {id}</Text>
+
+    const { id } = useLocalSearchParams();
+    const product = products.find((p) => p.id.toString() == id);
+    const sizes = ["S", "M", "L", "XL"]
+
+    const [selectedSize,setSselectedSize] = useState("M");
+
+    const AddToCart = () =>{
+        
+    }
+    return (
+        <View style={styles.container}>
+            <Stack.Screen options={{ title: product?.name }} />
+
+            <Image source={{ uri: product?.image }} style={styles.image} />
+            <Text>Select size</Text>
+            <View style={styles.sizesContainer}>
+                {sizes.map(size => {
+                    return (
+                        <Pressable onPress={() => setSselectedSize(size)}  style={[styles.size,{backgroundColor:size == selectedSize ? "grey":"white"}]} key={size} >
+                            <Text style={[styles.sizeText,{color: size == selectedSize ? "black":"grey"}]}>
+                                {size}
+                            </Text>
+                        </Pressable>
+                    )
+                })}
+            </View>
+            <Text style={styles.price}>R{product?.price}</Text>
+            <Button onPress={AddToCart} text="Add to cart"/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        aspectRatio: 1,
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    price: {
+        fontSize: 10,
+        fontWeight: 'bold',
+
+    },
+    sizesContainer:{
+            flexDirection: "row",
+            padding:10,
+            justifyContent: "space-around"
+    },
+    size:{
+        padding:20,
+        borderRadius: 25,
+    },
+    sizeText:{
+        fontSize:20,
+        fontWeight:'500',
+    }
+})
