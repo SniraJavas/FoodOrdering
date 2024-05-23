8d5077a1d055e584762ecef2beb0f1fe97ba92ca
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-05-23 02:31:12 +0200

    Custom components and tsconfig enabled and applied

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,49 @@
+import { StyleSheet, Image, Text, View } from 'react-native';
+import products from '../../assets/data/products';
+import Colors from '../constants/Colors';
+import {Product} from '../types';
 
 
-import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 export const defaultPizzaImage =
   'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
+  type ProductListItemProps = {
+        product:Product
+  };
 
-export default function ProductListItem({  }){
+export default function ProductListItem({product}:ProductListItemProps){
   return ( 
-        null
+    <View style={styles.container}>
+    <Image
+      source={{ uri: product.image || defaultPizzaImage }}
+      style={styles.image}
+      resizeMode="contain"
+    />
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price}>${product.price.toFixed(2)}</Text>
+  </View>
   );}
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 10,
+      overflow: 'hidden',
+    },
+    image: {
+      width: '100%',
+      aspectRatio: 1,
+      alignSelf: 'center',
+    },
+    title: {
+      fontWeight: '600',
+      fontSize: 18,
+      marginVertical: 10,
+    },
+    price: {
+      color: Colors.light.tint,
+      fontWeight: 'bold',
+      marginTop: 'auto',
+    },
+  });
